4308f3fb86e28412236be06780cefe3d49621294
Geert Jan Bex <geertjan.bex@uhasselt.be>
2023-05-05 10:28:28 +0200

    Add illustration of race condition

--- source-code/OpenMP/Pi/README.md
+++ source-code/OpenMP/Pi/README.md
@@ -3,6 +3,7 @@
 ## Purpose
 
 Illustration of
+* race conditions
 * `omp parallel`,
 * `omp for`,
 * `omp critical`
@@ -30,6 +31,8 @@ Use a `reduction` clause rather than `omp critical`.
 
 1. `pi_serial.c`: C implementation of the serial starting point.
 1. `pi_serial.f90`: Fortran implementation of the serial starting point.
+1. `pi_race_condition.c`: C implementation using OpenMP with a race condition.
+1. `pi_race_condition.f90`: Fortran implementation using OpenMP with a race condition.
 1. `pi_critical.c`: C implementation, uses `omp critical`.
 1. `pi_critical.f90`: Fortran implementation, uses `omp critical`.
 1. `pi_bad_critical.c`: C implementation, illustration of how *not* to use `omp critical`.
---
+++ source-code/OpenMP/Pi/pi_race_condition.f90
@@ -0,0 +1,28 @@
+program pi
+    use, intrinsic :: iso_fortran_env, only : DP => REAL64
+    implicit none
+    real(kind=DP) :: pi_value = 0.0_DP, dx, x
+    integer :: i, nr_points = 10000
+    character(len=1024) :: buffer
+
+    if (command_argument_count() > 1) then
+        call get_command_argument(1, buffer)
+        read (buffer, *) nr_points
+    end if
+
+    dx = 1.0_DP/nr_points
+
+    !$omp parallel default(none) shared(pi_value, dx, nr_points) private(x)
+    !$omp do
+    do i = 0, nr_points - 1
+        x = i*dx
+        pi_value = pi_value + 1.0_DP/(1.0_DP + x**2)
+    end do
+    !$omp end do
+    !$omp end parallel
+
+    pi_value = 4.0_DP*dx*pi_value
+
+    print '(A, F15.8)', 'pi = ', pi_value
+
+end program pi
